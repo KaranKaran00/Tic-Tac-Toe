b1b86f7b212056674cd90e0f63a507f05089e77e
KaranKaran00 <kharakaran504@gmail.com>
2026-02-06 20:54:33 +0530

    js add

--- app.js
+++ app.js
@@ -24,5 +24,16 @@ boxes.forEach((box) =>{
             box.innerText="X";
             turn0=true;
         }
-    })
-})
+        box.disabled =true;
+
+        checkWinner();
+    });
+});
+
+const checkWinner = () =>{
+    for(let pattern of winPatterns){
+        console.log(pattern);
+    }
+
+
+}
